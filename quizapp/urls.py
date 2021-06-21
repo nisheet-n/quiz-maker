846d7ee39294500763c2b53ebf3e0edb0b403417
Nisheet <nisheet2000@gmail.com>
2021-06-21 21:59:00 +0530

    Created Basic Quiz Functionalities

---
+++ quizapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.index, name='index'),
+	path('show/', views.show, name='show'),
+	path('create/', views.create, name='create'),
+	path('quiz/', views.quiz, name='quiz'),
+	path('result/', views.result, name='result'),
+	path('about/', views.about, name='about'),
+]
